a6ce7efebc57eb8606b1e3c95eda25e7cfffce5f
azamatdoronov <aza3el@inbox.ru>
2022-08-23 13:05:52 +0600

    Переписал CRUD для товара на классовые представления: ListView, DetailView, CreateView, UpdateView и DeleteView.

---
+++ webapp/views/products.py
@@ -0,0 +1,48 @@
+from django.urls import reverse_lazy, reverse
+from django.views.generic import UpdateView, DeleteView, DetailView, CreateView
+from webapp.views.base_view import SearchView
+
+from webapp.forms import ProductForm
+from webapp.models import Product
+
+
+class IndexView(SearchView):
+    model = Product
+    template_name = 'product/index.html'
+    ordering = ['category', 'p_name']
+    search_fields = ['name__icontains']
+    paginate_by = 6
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        return super().get_queryset().filter(balance__gt=0)
+
+
+class ProductView(DetailView):
+    model = Product
+    template_name = 'product/product_view.html'
+    queryset = Product.objects.filter(balance__gt=0)
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'product/product_create.html'
+
+    def get_success_url(self):
+        return reverse('webapp:product_view', kwargs={'pk': self.object.pk})
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'product/product_update.html'
+
+    def get_success_url(self):
+        return reverse('webapp:product_view', kwargs={'pk': self.object.pk})
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'product/product_delete.html'
+    success_url = reverse_lazy('webapp:index')
